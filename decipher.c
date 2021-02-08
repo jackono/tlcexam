f97c98d5cc9f023f1503366552c6da392be9e8c9
Jackono Fabella <jackonofabella@gmail.com>
2021-02-08 18:11:49 +0800

    fixed filename

--- decipher.c
+++ decipher.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
+int hex_to_int(char c){
+        int first = c / 16 - 3;
+        int second = c % 16;
+        int result = first*10 + second;
+        if(result > 9) result--;
+        return result;
+}
+
+int hex_to_ascii(char c, char d){
+        int high = hex_to_int(c) * 16;
+        int low = hex_to_int(d);
+        return high+low;
+}
+
 char decipher(int key)
 {
     switch(key)
@@ -27,10 +41,17 @@ char decipher(int key)
 int main()
 {
     char secret[1000] = "TQTHIKUIUNUQUXUFUITKTEIUUXTETQIUUIUNUQUXUFUNURICUIUQUXUYIUUIUYIUHH";
-
+    char buf = 0;
+    char str;
     for(int i = 0; i<strlen(secret);i++)
     {
-        printf("%c", decipher(secret[i]));
+        str = decipher(secret[i]);
+        if(i % 2 != 0){
+            printf("%c", hex_to_ascii(buf, str));
+        }else{
+            buf = str;
+        }
     }
     return 0;
 }
+
